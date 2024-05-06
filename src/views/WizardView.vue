7ae33378e0808340b526f6b7eabf2122cef32f93
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-06 18:08:29 +0330

    Update WizardView.vue

--- src/views/WizardView.vue
+++ src/views/WizardView.vue
@@ -5,6 +5,15 @@ import TheDescription from "@/components/Description.vue";
 
 <template>
   <div class="py-12">
+    <div class="container mx-auto px-4 mb-12">
+      <a href="https://github.com/amirrr1987/wizard-form">
+        link => https://github.com/amirrr1987/wizard-form
+        <img
+          class="w-12 h-12"
+          src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
+        />
+      </a>
+    </div>
     <div class="container mx-auto px-4 grid grid-cols-2 gap-4">
       <TheWizard />
       <TheDescription />
