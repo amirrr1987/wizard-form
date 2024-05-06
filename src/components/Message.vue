6e4f6e98b82b2be394f217509ce1dea603086e72
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-06 16:52:41 +0330

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="mt-2 text-sm text-red-600" v-if="props.message">
-    <span class="font-medium">Oops!</span> {{ props.message }}
+    {{ props.message }}
   </p>
 </template>
 <script setup>
