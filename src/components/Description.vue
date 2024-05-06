b39f0db09321702447065f0aa7c165eb99a7e377
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-06 17:17:42 +0330

    :poop: Write bad code that needs to be improved.

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <!-- <form>
     <div class="grid gap-6 mb-6 md:grid-cols-2">
       <div>
         <label
@@ -15,23 +15,47 @@
           v-model="description"
           id="description"
           type="text"
-          @input="isBalanced"
+          @input="debouncedIsBalanced"
         />
 
-        <p
-          class="mt-2 text-sm"
-          :class="isBalancedValue ? 'text-green-600' : 'text-red-600'"
-          v-if="description"
-        >
-          The text is <template v-if="!isBalancedValue">not</template> balanced.
+        <p class="mt-2 text-sm text-green-600" v-if="!isBalancedValue">
+          The text is balanced.
+        </p>
+        <p class="mt-2 text-sm text-red-600" v-else>
+          The text is not balanced.
         </p>
       </div>
+    </div>
+  </form> -->
+  <form>
+    <div class="grid">
+      <label
+        for="description"
+        class="block mb-2 text-sm font-medium text-gray-900"
+      >
+        Description
+      </label>
+      <input
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        id="description"
+        type="text"
+        v-model="description"
+        @input="debouncedIsBalanced"
+      />
+      <p
+        class="mt-2 text-sm text-green-600"
+        :class="isBalancedValue ? 'text-green-600' : 'text-red-600'"
+        v-if="description"
+      >
+        The text is<template v-if="!isBalancedValue"> not</template> balanced.
+      </p>
     </div>
   </form>
 </template>
 
 <script>
 import { useRegex } from "@/utils/regex";
+import { debounce } from "lodash";
 
 export default {
   data() {
@@ -42,6 +66,9 @@ export default {
     };
   },
   methods: {
+    debouncedIsBalanced: debounce(function () {
+      this.isBalanced();
+    }, 300),
     isBalanced() {
       this.isBalancedValue = !this.regex.chars.test(this.description);
     },
