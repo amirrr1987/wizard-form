947f28d307c7eb6f00cccfeafe2241b0861bcec4
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-07 18:12:51 +0330

    :bug: email

--- src/components/EmailStep.vue
+++ src/components/EmailStep.vue
@@ -6,7 +6,7 @@
     <input
       :value="props.person.email"
       @input="onInput"
-      type="email"
+      type="text"
       id="email"
       name="email"
       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,19 +1,26 @@
 export const useRegex = () => {
-  const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim;
-  const usernameRegex = /^(?:[a-zA-Z0-9_]{4,20})?$/;
-  const charsRegex = /[\[\]{}\(\)]/g;
+  const email = /[A-Z0-9._%+-]+@[A-Z0-9-]+\.[A-Z]{2,4}/gim;
+  const username = /^(?:[a-zA-Z0-9_]{4,20})?$/;
+  const chars = /[\[\]{}\(\)]/g;
 
   const isValidEmail = (value: string) => {
-    return value.length > 0 && emailRegex.test(value);
+    return value.length > 0 && email.test(value);
   };
 
   const isValidUsername = (value: string) => {
-    return value.length > 0 && usernameRegex.test(value);
+    return value.length > 0 && username.test(value);
   };
 
   const isValidChars = (value) => {
-    return value.length > 0 && charsRegex.test(value);
+    return value.length > 0 && chars.test(value);
   };
 
-  return { isValidEmail, isValidUsername, isValidChars };
+  return {
+    isValidEmail,
+    isValidUsername,
+    isValidChars,
+    email,
+    username,
+    chars,
+  };
 };
